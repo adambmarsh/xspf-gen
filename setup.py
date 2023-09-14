efcb8b42a10db6eed631eb794e577c15cbf931da
Adam Bukolt <abukolt@zoho.com>
2023-09-14 20:24:26 +0100

    update notifier library and incrment patch version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = [
 setuptools.setup(
     name="xspfgen",
     packages=setuptools.find_packages(),
-    version="0.1.3",
+    version="0.1.4",
     author="Adam Bukolt",
     author_email="abukolt@gmx.com",
     description="Package to create an XSPF playlist (Vlc compatible)",
